dcfb2a134cb227445f923d216a871cf1bdf9db80
truong29082001 <truongpx.10.student@fit.tdc.edu.vn>
2021-05-30 11:27:19 +0700

    commit week 10 sql

---
+++ Week10/sql truy van.sql
@@ -0,0 +1,22 @@
+1 SELECT * FROM teacher
+2 select * from student
+3 select * from course
+
+4
+5
+6 SELECT company.company_id,company.company_name
+FROM ((company
+INNER JOIN student ON company.company_id = student.company_id)
+INNER JOIN teacher ON teacher.user_id  = ....)
+7 select * from diary john student where student_name='Nguyen Van A
+8 chua làm xong
+9  select nhatky_ngay.giangvien_xet from nhatky_ngay join nhatky 
+    on nhatky_ngày.id_nhatky = nhatky.id_nhatky
+    join sinhvien on nhatky.sinhvien_id = sinhvien.sinhvien_id 
+    where sinhvien.tensinhvien = "Nguyen van b" && nhatky.namhoc = "2020-2021" && nhatky.hocky = 1
+10  select nhatky_ngay.trainerNhânXét from nhatky_ngay join from nhatky_ngay join nhatky 
+	on nhatky_ngày.id_nhatky = nhatky.id_nhatky
+	join sinhvien on nhatky.sinhvien_id = sinhvien.sinhvien_id 
+	where sinhvien.tensinhvien = "Nguyen van b" && nhatky.namhoc = "2020-2021" && nhatky.hocky = 1
+
+
